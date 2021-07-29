353deea139c079bdd28e724129e49b07c1e6ff1d
Peter <schmidt.multimedia@gmail.com>
2021-07-29 12:31:44 +0200

    02 / IF

--- script_01.js
+++ script_01.js
@@ -5,7 +5,7 @@ let ageJohn, ageMark;
 let isJohnOlder, isJohnEqual;
 
 // Assigment (Wertzuweisung)
-ageJohn = 30;
+ageJohn = 25;
 ageMark = 30;
 
 // Test | Logische Aussage
@@ -15,3 +15,18 @@ isJohnEqual = (ageJohn == ageMark);
 // Ausgabe
 console.log("isJohnOlder: " + isJohnOlder);
 console.log("isJohnEqual: " + isJohnEqual);
+
+/************ IF  ************/
+// TINA --> There is no alternative!
+// entweder JA oder nix ... alternativlos
+
+// if (true) 
+// if (false) 
+// if(isJohnOlder)
+if(ageJohn > ageMark)
+{
+    console.log("John ist älter.");
+}
+
+
+
