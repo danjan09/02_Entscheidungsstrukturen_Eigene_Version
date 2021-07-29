41e69e580d14e419af7b48334cca990a215c7e42
danjan09 <janzen.daniel@gmail.com>
2021-07-29 14:33:26 +0200

    Fallunterscheidung / CASE|SWITCH 1

--- script_01.js
+++ script_01.js
@@ -1,20 +1,20 @@
 // Entscheidungsstrukturen | control structures
 
 //Deklaration
-let ageJohn, ageMark;
-let isJohnOlder, isJohnEqual;
+// let ageJohn, ageMark;
+// let isJohnOlder, isJohnEqual;
 
 // Assignment (Wertzuweisung)
-ageJohn = 30;
-ageMark = 30;
+// ageJohn = 35;
+// ageMark = 30;
 
 // Test | Logische Aussage
-isJohnOlder = (ageJohn > ageMark);
-isJohnEqual = (ageJohn == ageMark);
+// isJohnOlder = (ageJohn > ageMark);
+// isJohnEqual = (ageJohn == ageMark);
 
 // Ausgabe
-console.log("isJohnOlder: " + isJohnOlder);
-console.log("isJohnEqual: " + isJohnEqual);
+// console.log("isJohnOlder: " + isJohnOlder);
+// console.log("isJohnEqual: " + isJohnEqual);
 
 /************ IF ************/
 //TINA --> There is no alternative!
@@ -35,19 +35,67 @@ console.log("isJohnEqual: " + isJohnEqual);
 // mit Alternative
 // entweder ja oder nein
 
-if (isJohnOlder) 
-{   
-    console.log("John ist älter.");    // true. auch Ja-Zweig genannt
-} 
-else 
-{
-    console.log("John ist jünger.");    // false. auch Nein-Zweig genannt
-}
+// if (isJohnOlder) 
+// {   
+//     console.log("John ist älter.");    // true. auch Ja-Zweig genannt
+// } 
+// else 
+// {
+//     console.log("John ist jünger.");    // false. auch Nein-Zweig genannt
+// }
 
 /************ Ternäre (ternary) Schreibweise ************/
 // console.log( (isJohnOlder) ? "John is older" : "Mark is older");
 
 
+/************ IF - ELSE - IF ************/
+// mit alternativen Fällen (älter, jünger, gleich alt)
+
+// if (isJohnOlder)                // true. auch Ja-Zweig genannt
+// {   
+//     console.log("John ist älter.");    
+// }                  // 1. alternative
+// else if(isJohnEqual)
+// {
+//     console.log("John ist gleich alt.");    
+// }                 // ggf. weiter Alternativen ..
 
 
+// else                            // false. auch Nein-Zweig genannt
+// {
+//     console.log("John ist jünger.");    
+// }
 
+
+/************ Fallunterscheidung / CASE|SWITCH 1 ************/
+
+const firstName = "Jane";
+let job;
+job = "driver";     // .. fährt Taxi!
+job = "diver";      // .. taucht im Rhein! 
+job = "artist";     // .. malt ein Bild! 
+job = "pilot";      // .. macht etwas anderes!
+
+job = "teacher";        // .. unterrichtet!
+job = "instructor";     // .. unterrichtet!     
+
+switch (job)
+{
+    case "driver":                                      // 1. Fall (case)
+        console.log(firstName + " fährt Taxi!");
+        break;
+    case "diver":                                       // 2. Fall (case)
+        console.log(firstName + " taucht im Rhein!");
+        break;
+    case "artist":                                      // 3. Fall (case)
+        console.log(firstName + " malt ein Bild!");
+        break;
+    case "teacher":                                 // 4. Fall (case)
+    case "instructor":                              // 4a. Fall (case)       
+    console.log(firstName + " unterrichtet!");
+        break;    
+    default:            // Für alle nichtbezeichneten Fälle     |   WICHTIG !
+            console.log(firstName + " macht etwas anderes!")
+        break;
+
+}
