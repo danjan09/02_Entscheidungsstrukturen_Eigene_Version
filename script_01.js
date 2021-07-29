470ff65e1499c1f95359fe57edd26274db10caf5
Peter <schmidt.multimedia@gmail.com>
2021-07-29 12:05:57 +0200

    01 / isOlder | isEqual

--- script_01.js
+++ script_01.js
@@ -1,2 +1,17 @@
 // Entscheidungsstrukturen | control structures
 
+// Deklaration
+let ageJohn, ageMark;
+let isJohnOlder, isJohnEqual;
+
+// Assigment (Wertzuweisung)
+ageJohn = 30;
+ageMark = 30;
+
+// Test | Logische Aussage
+isJohnOlder = (ageJohn > ageMark);
+isJohnEqual = (ageJohn == ageMark);
+
+// Ausgabe
+console.log("isJohnOlder: " + isJohnOlder);
+console.log("isJohnEqual: " + isJohnEqual);
